93d1c2abc5f053c608c305d10f497574ed23a078
Dadzial <duszakdamian034@gmail.com>
2026-01-04 17:04:22 +0100

    Login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css"
+
+export default function Login () {
+    return (
+        <div className={styles.page}>
+            <h1>Welcome !!!</h1>
+        </div>
+    );
+}
